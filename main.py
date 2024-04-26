232eba3c76e98e0b9d1f28735449cb934a39f7e9
Tan <tanthai028@gmail.com>
2024-04-26 13:49:03 -0400

    adding user instructions

--- main.py
+++ main.py
@@ -17,5 +17,6 @@ if __name__ == "__main__":
     clear_screen()
     db = Database('school_database.db')
     db.setup_database()  # Setup the initial database
+    print('* Press Ctrl+C to cancel operation *\n')
     main_menu(db)
     db.close()
