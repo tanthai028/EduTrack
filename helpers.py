f4888b268b56d1f5c48bcee2d4c0fc2f8d2ae5ab
Tan <tanthai028@gmail.com>
2024-04-26 16:56:48 -0400

    moved import data to database class

--- helpers.py
+++ helpers.py
@@ -1,6 +1,5 @@
 import os
 import platform
-import csv
 
 def clear_screen():
     """Clear the console based on the operating system."""
@@ -9,12 +8,3 @@ def clear_screen():
     else:
         os.system('clear')  # Clears the console for Unix/Linux/MacOS
 
-def import_data(db, csv_path, table, columns):
-    with open(csv_path, newline='', encoding='utf-8') as csvfile:
-        reader = csv.DictReader(csvfile)
-        for row in reader:
-            data_tuple = tuple(row[col] if col in row else None for col in columns)
-            placeholders = ', '.join(['?'] * len(columns))
-            # Use INSERT OR IGNORE to avoid inserting duplicate rows
-            sql = f'INSERT OR IGNORE INTO {table} ({", ".join(columns)}) VALUES ({placeholders});'
-            db.execute_query(sql, data_tuple)
